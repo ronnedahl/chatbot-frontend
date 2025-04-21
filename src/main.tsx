8483b61801be7e273203b0acf6b69cbe697bf8d3
Peter Andersson <karlstad88@hotmail.com>
2025-04-21 18:32:21 +0200

    first init commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import AdminPage from './pages/AdminPage.tsx'
+// import AdminPage from './pages/AdminPage.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AdminPage/>
+  </StrictMode>,
+)
